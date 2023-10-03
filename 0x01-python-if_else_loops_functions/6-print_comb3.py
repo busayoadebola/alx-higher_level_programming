83869d1e0a966c9078550136732ff829d2453e15
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-03 06:55:44 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for first_no in range(10):
     for second_no in range(first_no + 1, 10):
-        print("{:02d}, ".format(first_no * 10 + second_no), end="")
-print("\n")
-
+        print("{:02d}".format(first_no * 10 + second_no), end=", "
+              if first_no < 8 or second_no < 9 else "\n")
