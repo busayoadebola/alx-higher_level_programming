757921dd3b7b9caf831aa705c11d004feb9e9e38
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-03 07:23:01 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    converted = ""
+    for char in str:
+        if "a" <= char <= "z":
+            """ Convert from lower case to uppercase """
+            converted += chr(ord(char) - ord("a") + ord("A"))
+        else:
+            converted += char
+    print("{}\n".format(converted))
