42f061441d10a8e0e8107163eb1b108745ac2b63
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-03 02:26:00 -0700

    second correction

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def mes_body():
     message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
     sys.stderr.write(message + '\n')
     sys.exit(1)
+
 
 if __name__ == "__main__":
     mes_body()
