58d412ab632a6a786b51e156b43bd8420333ef49
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-05 06:00:24 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv[1:]
+    num_arguments = len(argv)
+
+if num_arguments == 0:
+    print("0 arguments")
+else:
+    if num_arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_arguments))
+
+    for i, arg in enumerate(argv, start=1):
+        print("{}: {}".format(i + 1, arg))
