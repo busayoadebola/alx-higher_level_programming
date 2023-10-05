e3c32d2463095109ad7b4da553db7194765df644
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-05 06:07:56 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     num_arguments = len(argv)
 
-if num_arguments == 0:
-    print("0 arguments")
-else:
-    if num_arguments == 1:
-        print("1 argument:")
+    if num_arguments == 0:
+        print("0 arguments")
     else:
-        print("{} arguments:".format(num_arguments))
+        if num_arguments == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(num_arguments))
 
-    for i, arg in enumerate(argv, start=1):
-        print("{}: {}".format(i, arg))
+        for i, arg in enumerate(argv, start=1):
+            print("{}: {}".format(i, arg))
