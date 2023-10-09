bfd171b72cf2c1568366b9bcba4ad488dd248d34
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-09 14:16:49 -0700

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for list_ver in range(len(my_list)):
+    if isinstance(my_list, list):
         my_list.reverse()
-        print("{:d}".format(my_list[list_ver]))
+        for list_ver in my_list:
+            print("{:d}".format(list_ver))
