4a162445e3a6442c43962146e098ce39209cd46d
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-24 13:42:59 -0700

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Division by zero is not allowed.")
+        result = None
+    except Exception as e:
+        print("An error occurred:", e)
+        result = None
+    finally:
+        if result is not None:
+            print("Inside result: {:.1f}".format(result))
+        return result
