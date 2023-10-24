a90471328333809678943e940854b83456d8a506
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-24 13:35:55 -0700

    changes in 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            try:
-                formatted_value = "{:d}".format(my_list[i])
-                print(formatted_value, end=' ')
-                count += 1
-            except (ValueError, TypeError):
-                pass
-    except IndexError:
-        pass
-    finally:
-        print()
-    return count
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    Return
