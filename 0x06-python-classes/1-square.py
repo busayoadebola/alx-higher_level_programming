c6851390ca48f79776d732862dd980124871f218
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-24 15:18:38 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Create a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square of type int.
+        """
+        self.__size = size
