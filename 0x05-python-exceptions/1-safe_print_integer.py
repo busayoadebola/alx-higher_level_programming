15f15b21913a50dfc9afc385b78bacd7bb453501
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-24 12:58:49 -0700

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@ def safe_print_integer(value):
     try:
         formatted_value = "{:d}".format(value)
         print(formatted_value)
-        print()
         return True
     except (ValueError, TypeError):
         return False
