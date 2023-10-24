3bc81756fd98a05b2bf5eac54aa93eb8f8c9bd99
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-24 14:47:01 -0700

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     printed_elements = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end=" ")
+            print("{}".format(my_list[i]), end="")
             printed_elements += 1
     except IndexError:
-        break
-    print("")
+        pass
+    print()
     return printed_elements
