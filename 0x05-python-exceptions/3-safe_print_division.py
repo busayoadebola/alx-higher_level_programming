f71bf80faff41286d2b42e0c9bba6858ec6409da
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-24 13:54:07 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,15 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
-        print("Division by zero is not allowed.")
-        result = None
-    except Exception as e:
-        print("An error occurred:", e)
+    except (ZeroDivisionError):
         result = None
     finally:
-        if result is not None:
-            print("Inside result: {}".format(result))
-        else:
-            print("Inside result: None")
-        return result
+        print("Inside result: {}".format(result))
+    return result
