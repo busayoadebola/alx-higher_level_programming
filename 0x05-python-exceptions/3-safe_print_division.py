3a47afe9c9a6ceb937abb832d8cb78ed48992944
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-24 13:46:22 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -11,4 +11,6 @@ def safe_print_division(a, b):
     finally:
         if result is not None:
             print("Inside result: {:.1f}".format(result))
+        else:
+            print("Inside result: None")
         return result
